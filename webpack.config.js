5ccb770efb6cd93ac7de02399e38da40a0d4514b
Anørak <seb@iamseb.dev>
2023-06-04 18:23:15 +0200

    Correction Webpack Images Hash + updated .env for ubuntu session

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ Encore
 
 	.copyFiles({
 		from: './assets/images',
-		to: 'images/[path][name].[hash:8].[ext]',
+		to: 'images/[path][name].[ext]',
+		// to: 'images/[path][name].[hash:8].[ext]',
 		pattern: /\.(png|jpg|jpeg)$/,
 	})
 	// public path used by the web server to access the output path
